b1ece823adea47f2ea0f7593c6abdeed1782510f
TejasAsawale <tejasasawale2607@gmail.com>
2024-09-04 22:51:39 +0530

    adding middleware functionality

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,18 +1,17 @@
 const User = require('../models/userModel');
+const asyncHandler = require("express-async-handler");
 
-const createUser = async(req,res) => {
+const createUser = asyncHandler(async (req,res) => {
     const email = req.body.email;
     const findUser = await User.findOne({ email: email });
     if (!findUser) {
         // create new user 
         const newUser = await User.create(req.body);
+        res.json(newUser);
     } else {
         // user already exists
-        res.json({
-            msg:"User Already Exists",
-            success: false,
-        })
+        throw new Error("User Already Exists");
     }
-};
+});
 
 module.exports = {createUser};
