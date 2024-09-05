a584f47a101f8ac63f0e50be2e9a7c9e829e824b
TejasAsawale <tejasasawale2607@gmail.com>
2024-09-05 18:42:52 +0530

    adding login and get all user functionality

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,3 +1,4 @@
+const { generateToken } = require('../config/jwtToken');
 const User = require('../models/userModel');
 const asyncHandler = require("express-async-handler");
 
@@ -14,4 +15,37 @@ const createUser = asyncHandler(async (req,res) => {
     }
 });
 
-module.exports = {createUser};
+const loginUser = asyncHandler(async(req,res) => {
+    const {email, password} = req.body;
+    // check if user exists or not 
+    const findUser = await User.findOne({ email  });
+    if (findUser && await findUser.isPasswordMatched(password)) {
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id),
+        });
+    } else {
+        throw new Error("Invalid Credentials");
+    }
+});
+
+// get all users
+const getallUser = asyncHandler(async(req,res)=> {
+    try {
+        const getUsers = await User.find();
+        res.json(getUsers);
+    } catch (error) {
+        throw new Error(error)
+    } 
+});
+
+module.exports = 
+{
+    createUser,
+    loginUser,
+    getallUser
+};
--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const { createUser } = require('../controller/userController');
+const { createUser, loginUser, getallUser } = require('../controller/userController');
 const router = express.Router();
 
 router.post('/register',createUser);
+router.post('/login',loginUser);
+router.get('/all-users',getallUser);
+
 module.exports = router;
