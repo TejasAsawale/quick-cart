675fba42fbfc3b3888226d8897c2a05f9328e2ad
TejasAsawale <tejasasawale2607@gmail.com>
2024-09-10 21:28:17 +0530

    adding authorization

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const { createUser, loginUser, getallUser, getaUser, deleteaUser, updatedUser } = require('../controller/userController');
+const { 
+    createUser, 
+    loginUser, 
+    getallUser, 
+    getaUser, 
+    deleteaUser, 
+    updatedUser 
+} = require('../controller/userController');
+const { authMiddleware } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-router.post('/register',createUser);
-router.post('/login',loginUser);
-router.put('/:id',updatedUser);
-router.get('/all-users',getallUser);
-router.get('/:id',getaUser);
-router.delete('/:id',deleteaUser);
+router.post('/register', createUser);
+router.post('/login', loginUser);
+router.put('/:id', updatedUser);
+router.get('/all-users', getallUser);
+router.get('/:id', authMiddleware,  getaUser);
+router.delete('/:id', deleteaUser);
 
 module.exports = router;
