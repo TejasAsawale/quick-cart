e0975bc1d328b28d928122e2e2761d2cc2bdf589
TejasAsawale <tejasasawale2607@gmail.com>
2025-02-18 15:46:01 +0530

    Adding Productcards structure

---
+++ frontend/src/components/ProductCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ProductCard = () => {
+  return (
+    <div className='col-3'>
+        <div className='product-card'>
+            <div className='product-image'>
+                <img src='images/watch.jpg' alt='product img'/>
+            </div>
+            <div className='product-details'>
+                <h6 className='brand'>
+                    Havels
+                </h6>
+                <h5 className='product-title'>
+                    Kids headphones bulk 10 pack multi coloured for students
+                </h5>
+                <p className='price'>$100.00</p>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default ProductCard;
