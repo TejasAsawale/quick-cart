f95c63ac516f203259c4da76f9656800069918db
TejasAsawale <tejasasawale2607@gmail.com>
2025-02-20 22:05:13 +0530

    Set icons for product card

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
 
 const ProductCard = () => {
   return (
-    <div className='col-3'>
-        <div className='product-card'>
-            <div className='product-image'>
-                <img src='images/watch.jpg' alt='product img'/>
-            </div>
-            <div className='product-details'>
-                <h6 className='brand'>
-                    Havels
-                </h6>
-                <h5 className='product-title'>
-                    Kids headphones bulk 10 pack multi coloured for students
-                </h5>
-                <p className='price'>$100.00</p>
-            </div>
+    <div className="col-3">
+      <div className="product-card position-relative ">
+        <div className="wishlist-icon position-absolute">
+            <Link><img src="images/wish.svg" alt="wishlist"/></Link>
         </div>
+        <div className="product-image">
+          <img src="images/watch.jpg" alt="product img" />
+
+        </div>
+        <div className="product-details">
+          <h6 className="brand">Havels</h6>
+          <h5 className="product-title">
+            Kids headphones bulk 10 pack multi coloured for students
+          </h5>
+          <ReactStars
+            count={5}
+            size={24}
+            value="3"
+            edit={false}
+            activeColor="#ffd700"
+          />
+          <p className="price">$100.00</p>
+        </div>
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
+            <Link>
+              <img src="images/prodcompare.svg" alt="product compare" />
+            </Link>
+            <Link>
+              <img src="images/view.svg" alt="view" />
+            </Link>
+            <Link>
+              <img src="images/add-cart.svg" alt="add cart" />
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
