94ab56e18e8443f4b1a9da5ee3c9a58fb971a3b1
TejasAsawale <tejasasawale2607@gmail.com>
2025-03-17 11:11:36 +0530

    Adding login and forgot-password page

--- admin-app/src/components/MainLayout.js
+++ admin-app/src/components/MainLayout.js
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
-} from "@ant-design/icons";
-import { Button, Layout, Menu, theme } from "antd";
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
 const { Header, Sider, Content } = Layout;
-
 const MainLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
@@ -18,22 +20,22 @@ const MainLayout = () => {
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={["1"]}
+          defaultSelectedKeys={['1']}
           items={[
             {
-              key: "1",
+              key: '1',
               icon: <UserOutlined />,
-              label: "nav 1",
+              label: 'nav 1',
             },
             {
-              key: "2",
+              key: '2',
               icon: <VideoCameraOutlined />,
-              label: "nav 2",
+              label: 'nav 2',
             },
             {
-              key: "3",
+              key: '3',
               icon: <UploadOutlined />,
-              label: "nav 3",
+              label: 'nav 3',
             },
           ]}
         />
@@ -50,7 +52,7 @@ const MainLayout = () => {
             icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
             onClick={() => setCollapsed(!collapsed)}
             style={{
-              fontSize: "16px",
+              fontSize: '16px',
               width: 64,
               height: 64,
             }}
@@ -58,7 +60,7 @@ const MainLayout = () => {
         </Header>
         <Content
           style={{
-            margin: "24px 16px",
+            margin: '24px 16px',
             padding: 24,
             minHeight: 280,
             background: colorBgContainer,
@@ -71,5 +73,4 @@ const MainLayout = () => {
     </Layout>
   );
 };
-
 export default MainLayout;
